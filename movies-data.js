d4737bc408f3b484bb79b14addd68b4aa008f8b0
hansaka345 <dissanayakahansaka37@gmail.com>
2026-07-09 00:36:11 +0530

    Add Harry Potter episodes data and update categories
    Added EPISODES_DATA for Harry Potter films with video and download links. Updated trending category to include the new Harry Potter IMDb ID.

--- movies-data.js
+++ movies-data.js
@@ -30,6 +30,31 @@ export const EPISODES_DATA = {
     }
   }
 };
+  export const EPISODES_DATA = {
+  'tt0295297': { // Harry ptter IMDb ID
+    '3' { // film collection 
+      '1': { 
+        videoUrl: 'https://drive.google.com/file/d/1D0xcgiaJ10hUGAorH0cdRoKb3rxNTd8o/preview',
+        subtitleUrl: 'harrypotter.srt',
+        name: 'Episode 1: 3 films Premiere',
+        whatsappDownloadUrl: 'https://wa.me/94775062608?text=.download%20Harry%20Potter%201%20-%20https%3A%2F%2Fdrive.google.com%2Ffile%2Fd%2F1D0xcgiaJ10hUGAorH0cdRoKb3rxNTd8o%2Fpreview'
+
+      },
+      '2': { 
+        videoUrl: 'https://drive.google.com/file/d/14_9Xp-giND8ERkt8Jxw02kM9WbhrSakB/preview',
+        subtitleUrl: 'harry potter.srt', 
+        name: 'Film 2',
+        whatsappDownloadUrl: 'https://wa.me/94775062608?text=.download%20Harry%20Potter%202%20-%20https%3A%2F%2Fdrive.google.com%2Ffile%2Fd%2F14_9Xp-giND8ERkt8Jxw02kM9WbhrSakB%2Fpreview'
+        
+      '3': { 
+        videoUrl: 'https://drive.google.com/file/d/1f32MgHNhl5ajzO-5xcmtkUBVHfjRZry5/preview',
+        subtitleUrl: 'harrypotter.srt', 
+        name: 'Film 3',
+        whatsappDownloadUrl: 'https://wa.me/94775062608?text=.download%20Harry%20Potter%203%20-%20https%3A%2F%2Fdrive.google.com%2Ffile%2Fd%2F1f32MgHNhl5ajzO-5xcmtkUBVHfjRZry5%2Fpreview'
+       }
+     }
+  }
+};
 
 // Hardcoded Movies (චිත්‍රපට වීඩියෝ සහ බාගත කිරීම් ලින්ක්ස්)
 export const CUSTOM_MOVIES_DATA = {
@@ -44,18 +69,13 @@ export const CUSTOM_MOVIES_DATA = {
     subtitleUrl: 'obse.srt',
     name: 'Obsession (2026)',
     whatsappDownloadUrl: 'https://wa.me/94775062608?text=.download%20Obsession%20Film%20-%20https%3A%2F%2Fhub.airbender.buzz%2F8eda6e05bd1c6668fcc135d48c06590d%3Ftoken%3D1782922178139'
-  },
-  'tt0241527': { // Harry potter IMDB ID
-    videoUrl: 'https://drive.google.com/file/d/1D0xcgiaJ10hUGAorH0cdRoKb3rxNTd8o/preview',
-    SubtitleUrl: 'Sinhala.dubded',
-    name: 'Harry potter (2001)',
-    whatsappDownloadUrl: 'https://wa.me/94775062608?text=.download%20Harry%20Potter%201%20-%20https%3A%2F%2Fdrive.google.com%2Ffile%2Fd%2F1D0xcgiaJ10hUGAorH0cdRoKb3rxNTd8o%2Fpreview'
+    
   }
 
 };
 
 export const HERO_SERIES = [
-  {id:'tt0241527',title:'Harry Potter and the Sorcerer\'s Stone',year:'2001',rating:'7.6',plot:'An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world.'},
+  {id:'tt0295297',title:'Harry Potter and the Sorcerer\'s Stone',year:'2001',rating:'7.6',plot:'An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world.'},
   {id:'tt4633694',title:'Spider-Man: Into the Spider-Verse',year:'2018',rating:'8.4',plot:'Teen Miles Morales becomes the Spider-Man of his universe and must join with five spider-powered individuals from other dimensions to stop a threat for all realities.'},
   {id:'tt28212876',title:'Obsession',year:'2026',rating:'8.0',plot:'A thrilling and emotional deep dive into love, mystery, and unpredictable human desire.'},
   {id:'tt11198330',title:'House of the Dragon',year:'2022',rating:'8.5',plot:'The story of the Targaryen civil war that took place about 200 years before the events of Game of Thrones.'},
@@ -67,7 +87,7 @@ export const HERO_SERIES = [
 ];
 
 export const CATEGORIES = {
-  trending: ['tt0241527','tt4633694','tt28212876','tt11198330','tt9813792','tt13443470','tt10919420','tt4574334','tt0903747','tt8111088','tt3032476','tt0386676','tt0944947','tt0108778','tt2560140'],
+  trending: ['tt0295297','tt4633694','tt28212876','tt11198330','tt9813792','tt13443470','tt10919420','tt4574334','tt0903747','tt8111088','tt3032476','tt0386676','tt0944947','tt0108778','tt2560140'],
   hollywood: ['tt4633694','tt28212876','tt0111161','tt1375666','tt0816692','tt0468569','tt0137523','tt0109830','tt0120737','tt0167260','tt4154796','tt7286456','tt15398776','tt9362722'],
   kdrama: ['tt15266542','tt14689414','tt13370348','tt11280740','tt10262630','tt13443470','tt11198330','tt1190634','tt10850932','tt11212276'],
   bollywood: ['tt8178634','tt15327088','tt12735488','tt1187043','tt12844910','tt10648342','tt0451850','tt0816442','tt0066763','tt0347304'],
